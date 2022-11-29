1a0d78b1c696a114649cb1844773a4abd69f90f8
MacFlyOSX <b.tasaki@gmail.com>
2022-11-28 21:56:08 -0800

    added #67

--- 67-addBinary.js
+++ 67-addBinary.js
@@ -1,31 +1,24 @@
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
 
 
-var addBinary = function(a, b) {
-    let sum = 0;
-    Array.from(a).forEach((ele, i)=> ele === '1' ? sum += 2**(a.length - 1 - i) : null);
-    console.log('sum after array a', sum);
-    Array.from(b).forEach((ele, i)=> ele === '1' ? sum += 2**(b.length - 1 - i) : null);
-    console.log('sum after array b', sum);
-    let str = '';
-    let len = a.length > b.length ? a.length : b.length;
-    console.log('this is the len', len);
-    while (sum >= 0 && len >= 0) {
-        if (sum >= 2**len) {
-            console.log('in the sum > 2**len');
-            sum -= 2**len;
-            console.log('new sum', sum);
-            str += '1';
-            console.log('new str', str);
-        } else {
-            console.log('in else');
-            if (str.length) {
-                str += '0';
-                console.log('added the str 0', str);
-            }
-        }
-        len--;
-    }
-    return str;
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+*/
+
+const addBinary = function(a, b) {
+    let sum = BigInt('0b' + a) + BigInt('0b' + b);
+    return sum.toString(2);
 };
-
-console.log(addBinary('11', '1'))
