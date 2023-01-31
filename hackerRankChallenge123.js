cc20bbf39be034b2f8a431ef68038f95633e1453
MacFlyOSX <b.tasaki@gmail.com>
2023-01-31 09:33:20 -0800

    added hackerrank challenge 1-23 #2

--- hackerRankChallenge123.js
+++ hackerRankChallenge123.js
@@ -18,4 +18,27 @@ function commonSubstring(a, b) {
     }
 }
 
+// 2. Password Validation
 
+function getPasswordStrength(passwords, common_words) {
+    const result = [];
+
+    for (let word of passwords) {
+        console.log(word);
+        if (word.length < 6 || word === word.toLowerCase() || word === word.toUpperCase() || !isNaN(word))
+            result.push('weak');
+        else {
+            let bool = true;
+            for (let com of common_words) {
+                if (word.includes(com)) {
+                    result.push('weak');
+                    bool = false;
+                    break;
+                    }
+            }
+            if (bool) result.push('strong');
+        }
+    }
+
+    return result;
+}
